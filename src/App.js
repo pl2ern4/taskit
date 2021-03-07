55f506903ce7aed1de18c86ba08ae2f6ef29c381
pl2ern4 <chaturvediprerna03@gmail.com>
2021-03-07 13:41:06 +0530

    format document

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import React from 'react';
 import DragAndDrop from './components/drag_and_drop/DragAndDrop';
 import Profiles from './components/profiles/Profiles';
 
-import {reducer} from './reducer';
+import { reducer } from './reducer';
 import './App.css';
 
 function App() {
   const tasks = [
-    {description: "create a task", subject:"tasking nklnkln ndnkl nkl nlkllr", assignee:"Wca hgfh", category:'todo', importance:"yellow"},
-    {description: "create a task", subject:"tasking", assignee:"Tnsajnk bjvbk", category:'todo', importance:"yellow"},
-    {description: "create a task",subject:"tasking", assignee:"Zee eee", category:'working', importance:"red"},
-    {description: "create a task", subject:"tasking", assignee:"Zinat erfj", category:'done', importance:"pink"},
+    { description: "create a task", subject: "tasking nklnkln ndnkl nkl nlkllr", assignee: "Wca hgfh", category: 'todo', importance: "yellow" },
+    { description: "create a task", subject: "tasking", assignee: "Tnsajnk bjvbk", category: 'todo', importance: "yellow" },
+    { description: "create a task", subject: "tasking", assignee: "Zee eee", category: 'working', importance: "red" },
+    { description: "create a task", subject: "tasking", assignee: "Zinat erfj", category: 'done', importance: "pink" },
   ]
   const team = ["Tnsajnk bjvbk",
-  "Zinat erfj",
-  "Wca hgfh",
-  "Zee eee"]
+    "Zinat erfj",
+    "Wca hgfh",
+    "Zee eee"]
 
   const [data, dispatch] = React.useReducer(
     reducer, { tasks, team }
   )
 
-  const handleSubmit = params => {
-    debugger;
-    dispatch({type:'ADD_MEMBER',teamName: params.name});
-  }
+  const handleSubmit = params => dispatch({ type: 'ADD_MEMBER', teamName: params.name });
+
+  const deleteUser = params => dispatch({ type: 'DELETE_MEMBER', teamName: params });
 
   return (
     <>
-      <Profiles dispatch={dispatch} team={data.team} handleSubmit={handleSubmit}/>
-      <DragAndDrop dispatch={dispatch} team={data.team} tasks={data.tasks}/>
+      <Profiles deleteUser={deleteUser} team={data.team} handleSubmit={handleSubmit} />
+      <DragAndDrop dispatch={dispatch} team={data.team} tasks={data.tasks} />
     </>
   );
 }
--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -1,36 +1,37 @@
-import React,{useState} from 'react';
-import {Profile, ImageContainer, FullName, TeamContainer} from './ProfilesStyle';
-import {getName} from '../../utility';
+import React, { useState } from 'react';
+import { Profile, ImageContainer, FullName, TeamContainer, DeleteButton } from './ProfilesStyle';
+import { getName } from '../../utility';
 import AddNewMember from '../addnewmember/AddNewMember';
 
-const Profiles = ({team = [], handleSubmit}) => {
+const Profiles = ({ team = [], handleSubmit, deleteUser }) => {
     const [openForm, setOpenForm] = useState();
-    
+
     const isDuplicate = params => team.includes(params);
     return (<TeamContainer>
-        {team.map((obj,key)=>{
-        return (<Profile key={`profile-${key}`}>
-                        <ImageContainer>
-                            <span>{getName(obj)}</span>
-                            <FullName>{obj}</FullName>
-                        </ImageContainer>
-                    </Profile>)
-        })} 
+        {team.map((obj, key) => {
+            return (<Profile key={`profile-${key}`}>
+                <ImageContainer>
+                    <span>{getName(obj)}</span>
+                    <DeleteButton onClick={() => deleteUser(obj)}>x</DeleteButton>
+                    <FullName>{obj}</FullName>
+                </ImageContainer>
+            </Profile>)
+        })}
         <Profile key={`profile-new-member`}>
-            <ImageContainer onClick={()=>setOpenForm(!openForm)}>
+            <ImageContainer onClick={() => setOpenForm(!openForm)}>
                 <span>+</span>
                 <FullName>Add new member</FullName>
             </ImageContainer>
         </Profile>
-        {(openForm && 
-            <AddNewMember 
-                isAccessUser={(team.length>=10)} 
-                isDuplicate={isDuplicate} 
-                handleSubmit={handleSubmit} 
-                setOpenForm={setOpenForm} 
-                openForm={openForm}/>) || null}
-    </TeamContainer>)        
-    
+        {(openForm &&
+            <AddNewMember
+                isAccessUser={(team.length >= 10)}
+                isDuplicate={isDuplicate}
+                handleSubmit={handleSubmit}
+                setOpenForm={setOpenForm}
+                openForm={openForm} />) || null}
+    </TeamContainer>)
+
 }
 
 export default Profiles;
